b7c79c6d7a33ffa79a001621ef9dc22ba36ce68a
Stavros Anastasiadis <sanastasiadis@gmail.com>
2016-07-16 13:50:22 +0200

    FileNotFoundException throwing from the GenericSource interface

--- src/main/java/org/stavros/interplat/source/GenericSource.java
+++ src/main/java/org/stavros/interplat/source/GenericSource.java
@@ -1,12 +1,14 @@
 package org.stavros.interplat.source;
 
+import java.io.FileNotFoundException;
+
 import org.stavros.interplat.model.Model;
 
 public interface GenericSource {
 	
 	void connect();
 	
-	Model getData(GenericQuery query);
+	Model getData(GenericQuery query) throws FileNotFoundException;
 	
 	void close();
 
